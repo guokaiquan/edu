5cae2552d3a71983663866180268496c1144fcfc
guokaiquan <1830480018@qq.com>
2019-12-27 14:42:54 +0800

    首次提交

---
+++ src/main/java/com/edu/xueyuan_edu/XueyuanEduApplication.java
@@ -0,0 +1,13 @@
+package com.edu.xueyuan_edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueyuanEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueyuanEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/xueyuan_edu/XueyuanEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.xueyuan_edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XueyuanEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
